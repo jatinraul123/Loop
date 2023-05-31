84e9a6bc15a65f232ec204e1fb312e46a9826c15
Admin <Admin@DESKTOP-D0KCSOU>
2023-05-31 12:41:12 +0530

    update 3

--- loop/src/oneten/Loop.java
+++ loop/src/oneten/Loop.java
@@ -3,7 +3,7 @@ package oneten;
 public class Loop {
 	public static void main(String[] args) {
 		for (int i =0;i<=10;i++)
-		for (int o=2;o <=20;o++){
+		for (int o=5;o <=10;o++){
 			System.out.println(i);
 			
 			System.out.println(o);
